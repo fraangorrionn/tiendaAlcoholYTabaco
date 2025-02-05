7c11194929bb512171f3c09a6396002052bee0ba
Fran Rivera <francisco.rivera.gil.al@iespoligonosur.org>
2025-02-05 20:34:18 +0100

    Moviendo cambios a nueva rama para la búsqueda avanzada

--- tienda/tienda/api_urls.py
+++ tienda/tienda/api_urls.py
@@ -15,18 +15,19 @@ from tienda.api_view import (
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('api/productos/', lista_productos_api, name='lista_productos_api'),
-    path('api/productos/detallados/', lista_productos_detallada_api, name='lista_productos_detallada_api'),
-    path('api/ordenes/', lista_ordenes_api, name='lista_ordenes_api'),
-    path('api/proveedores/', lista_proveedores_api, name='lista_proveedores_api'),
-    path('api/reclamos/', reclamo_list_view, name='reclamo-list'),
+    path('productos/', lista_productos_api, name='lista_productos_api'),
+    path('productos/detallados/', lista_productos_detallada_api, name='lista_productos_detallada_api'),
+    path('ordenes/', lista_ordenes_api, name='lista_ordenes_api'),
+    path('proveedores/', lista_proveedores_api, name='lista_proveedores_api'),
+    path('reclamos/', reclamo_list_view, name='reclamo-list'),
 
     # Autenticación con JWT
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
-    path('api/productos/busqueda/', busqueda_simple_producto, name='busqueda_simple_producto'),
-    path('api/productos/busqueda-avanzada/', busqueda_avanzada_producto, name='busqueda_avanzada_producto'),
-    path('api/ordenes/busqueda-avanzada/', busqueda_avanzada_orden, name='busqueda_avanzada_orden'),
-    path('api/proveedores/busqueda-avanzada/', busqueda_avanzada_proveedor, name='busqueda_avanzada_proveedor'),
+    path('productos/busqueda/', busqueda_simple_producto, name='busqueda_simple_producto'),
+    path('productos/busqueda-avanzada/', busqueda_avanzada_producto, name='busqueda_avanzada_producto'),
+    path('ordenes/busqueda-avanzada/', busqueda_avanzada_orden, name='busqueda_avanzada_orden'),
+    path('proveedores/busqueda-avanzada/', busqueda_avanzada_proveedor, name='busqueda_avanzada_proveedor'),
+
 ]
